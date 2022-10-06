bc61d899363270a5e43bc073926f100895a0092b
Jesus Figuera <figueraj196@gmail.com>
2022-10-06 13:04:15 -0400

    Exception added to database connection

--- database/db.py
+++ database/db.py
@@ -1,47 +1,64 @@
+import pathlib
 import mysql.connector as database
-from utils import read_file
+from .utils import read_file
 from flask import g,current_app
 from flask.cli import with_appcontext
-
 import click
+import os
+import sys
 
-def get_db():
+
+def get_db() -> tuple:
+
+
+    """
+    Generate a connection with the database
+
+    return: cursor and database
+    
+    """
+
     if 'db' not in g:
-        g.db = database.connect(
-            host=current_app.config['DB_HOST'],
-            user=current_app.config['DB_USER'],
-            password=current_app.config['DB_PASSWORD'],
-            database=current_app.config['DB_NAME']
-        )
-        g.c = g.db.cursor(dictionary=True)
+        try:
+            g.db = database.connect(
+                host=current_app.config['DB_HOST'],
+                user=current_app.config['DB_USER'],
+                password=current_app.config['DB_PASSWORD'],
+                database=current_app.config['DB_NAME']
+            )
+            g.c = g.db.cursor(dictionary=True,buffered=True)
 
-        return g.db,g.c
+        except Exception as ex:
+            raise Exception(ex)
+        else:
+            return g.db,g.c
+        
 
-def close_db(e=None):
-    """"""
+def close_db(e=None) -> None:
     db = g.pop('db',None)
     if db is not None:
         db.close()
 
 
-def init_db():
+def init_db() -> None:
     """This function creates the tables for the database with a DDL code.
     
     Return: None
     """
-
-    db, cursor = get_db()
-
-    path = "/database/workstation_tables.sql"
-
-    sql_ddl = read_file(path)
+    path = sys.path[0] + "/database/workstation_tables.sql"
 
     try:
+        db, cursor = get_db()
+        sql_ddl = read_file(path)
         cursor.execute(sql_ddl)
         db.commit()
-
     except Exception as ex:
         raise Exception(ex)
+
+
+
+
+
 
 @click.command('init-db')
 @with_appcontext
