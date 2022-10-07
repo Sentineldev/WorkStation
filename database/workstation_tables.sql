bacd5e13322536253184ca34244579df4d8f17bf
Jesus Figuera <figueraj196@gmail.com>
2022-10-07 10:35:05 -0400

    Added sql alchemy to requirements

--- database/workstation_tables.sql
+++ database/workstation_tables.sql
@@ -7,13 +7,13 @@ birthdate DATE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS phone(
-person_id INT NOT NULL REFERENCES person(person_id),
+person_id INT REFERENCES person (person_id),
 phone_number VARCHAR(15) PRIMARY KEY
 );
 
 CREATE TABLE IF NOT EXISTS user(
 user_id INT AUTO_INCREMENT PRIMARY KEY,
-person_id INT NOT NULL REFERENCES person(person_id),
+person_id INT REFERENCES person (person_id),
 username VARCHAR(20) NOT NULL,
 password VARCHAR(16) NOT NULL
 );
